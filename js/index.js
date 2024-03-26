84cf850111d5d3b77f399a9f7496e737bef34fc3
lixr <lixr@cqnews.net>
2024-03-26 10:03:41 +0800

    update: style

--- js/index.js
+++ js/index.js
@@ -22,7 +22,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate("|#countdown 3||xuxiuting|我们|在一起吧|❤|#rectangle|lxr");
+      S.UI.simulate("|#countdown 3||XXX|祝你|生日快乐|❤|#time|lxr");
     }
 
     S.Drawing.loop(function () {
